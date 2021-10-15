msujew/pyright

commit 1e1a476845ed83eec18562cf9a2921b02a0f68e3

Show simple signatures. (#3)
This isn't intended for general use. It's a simplified mode suitable for
use with micro:bit.

packages/pyright-internal/src/languageService/signatureHelpProvider.ts
@@ -17,11 +17,13 @@ import * as ParseTreeUtils from '../analyzer/parseTreeUtils';
 import { getCallNodeAndActiveParameterIndex } from '../analyzer/parseTreeUtils';
 import { SourceMapper } from '../analyzer/sourceMapper';
 import { CallSignature, TypeEvaluator } from '../analyzer/typeEvaluatorTypes';
+import { FunctionParameter } from '../analyzer/types';
 import { throwIfCancellationRequested } from '../common/cancellationUtils';
 import { convertPositionToOffset } from '../common/positionUtils';
 import { Position } from '../common/textRange';
-import { CallNode, NameNode, ParseNodeType } from '../parser/parseNodes';
+import { CallNode, NameNode, ParameterCategory, ParseNodeType } from '../parser/parseNodes';
 import { ParseResults } from '../parser/parser';
+import { printSimplifiedFunctionSignature } from './completionProvider';
 import { getDocumentationPartsForTypeAndDecl, getFunctionDocStringFromType } from './tooltipUtils';
 
 export interface ParamInfo {
@@ -122,37 +124,43 @@ export class SignatureHelpProvider {
         format: MarkupKind
     ): SignatureInfo {
         const functionType = signature.type;
-        const stringParts = evaluator.printFunctionParts(functionType);
         const parameters: ParamInfo[] = [];
         const functionDocString =
             getFunctionDocStringFromType(functionType, sourceMapper, evaluator) ??
             this._getDocStringFromCallNode(callNode, sourceMapper, evaluator);
 
-        let label = '(';
-        const params = functionType.details.parameters;
-
-        stringParts[0].forEach((paramString: string, paramIndex) => {
-            let paramName = '';
-            if (paramIndex < params.length) {
-                paramName = params[paramIndex].name || '';
-            } else if (params.length > 0) {
-                paramName = params[params.length - 1].name || '';
+        // micro:bit-specific change to drop type information here
+        // We don't filter out defaulted params here as they could be active.
+        let label = functionType.details.fullName + '(';
+        const params = functionType.details.parameters.filter((p, index) => !(index === 0 && p.name === 'self'));
+        params.forEach((param: FunctionParameter, paramIndex) => {
+            let paramString: string = param.name || '';
+            if (param.category === ParameterCategory.VarArgList) {
+                paramString = '*' + paramString;
+            } else if (param.category === ParameterCategory.VarArgDictionary) {
+                paramString = '**' + paramString;
+            }
+            if (param.hasDefault && param.defaultValueExpression) {
+                paramString += ' = ';
+                paramString += ParseTreeUtils.printExpression(
+                    param.defaultValueExpression,
+                    ParseTreeUtils.PrintExpressionFlags.ForwardDeclarations
+                );
             }
 
             parameters.push({
                 startOffset: label.length,
                 endOffset: label.length + paramString.length,
                 text: paramString,
-                documentation: extractParameterDocumentation(functionDocString || '', paramName),
+                documentation: extractParameterDocumentation(functionDocString || '', paramString),
             });
 
             label += paramString;
-            if (paramIndex < stringParts[0].length - 1) {
+            if (paramIndex < params.length - 1) {
                 label += ', ';
             }
         });
-
-        label += ') -> ' + stringParts[1];
+        label += ')';
 
         let activeParameter: number | undefined;
         if (signature.activeParam) {

packages/pyright-internal/src/languageService/completionProvider.ts
@@ -2065,6 +2065,8 @@ export class CompletionProvider {
                         const type = this._evaluator.getEffectiveTypeOfSymbol(symbol);
                         if (type) {
                             let typeDetail: string | undefined;
+                            // micro:bit-specific change to drop type information here
+                            let altDetail: string | undefined;
                             let documentation: string | undefined;
 
                             switch (primaryDecl.type) {
@@ -2101,6 +2103,8 @@ export class CompletionProvider {
                                                 ': ' +
                                                 this._evaluator.printType(propertyType, /* expandTypeAlias */ false) +
                                                 ' (property)';
+                                            altDetail =
+                                                detail.boundObject.details.fullName + '.' + name + ' (property)';
                                         } else if (isOverloadedFunction(functionType)) {
                                             // 35 is completion tooltip's default width size
                                             typeDetail = getOverloadedFunctionTooltip(
@@ -2108,11 +2112,16 @@ export class CompletionProvider {
                                                 this._evaluator,
                                                 /* columnThreshold */ 35
                                             );
+                                            // Intentionally just the first, usually simplest one.
+                                            altDetail = printSimplifiedFunctionSignature(functionType.overloads[0]);
                                         } else {
                                             typeDetail =
                                                 name +
                                                 ': ' +
                                                 this._evaluator.printType(functionType, /* expandTypeAlias */ false);
+                                            if (isFunction(functionType)) {
+                                                altDetail = printSimplifiedFunctionSignature(functionType);
+                                            }
                                         }
                                     }
                                     break;
@@ -2197,12 +2206,19 @@ export class CompletionProvider {
                                     primaryDecl) as VariableDeclaration;
                                 documentation = getVariableDocString(decl, this._sourceMapper);
                             }
-
+                            if (!altDetail && !documentation) {
+                                // Bail if we have nothing.
+                                return;
+                            }
                             if (this._options.format === MarkupKind.Markdown) {
-                                let markdownString = '```python\n' + typeDetail + '\n```\n';
-
-                                if (documentation) {
+                                let markdownString = '';
+                                if (altDetail) {
+                                    markdownString += '```python\n' + altDetail + '\n```\n';
+                                }
+                                if (altDetail && documentation) {
                                     markdownString += '---\n';
+                                }
+                                if (documentation) {
                                     markdownString += convertDocStringToMarkdown(documentation);
                                 }
 
@@ -2213,10 +2229,14 @@ export class CompletionProvider {
                                     value: markdownString,
                                 };
                             } else if (this._options.format === MarkupKind.PlainText) {
-                                let plainTextString = typeDetail + '\n';
-
-                                if (documentation) {
+                                let plainTextString = '';
+                                if (altDetail) {
+                                    plainTextString += altDetail + '\n';
+                                }
+                                if (altDetail && documentation) {
                                     plainTextString += '\n';
+                                }
+                                if (documentation) {
                                     plainTextString += convertDocStringToPlainText(documentation);
                                 }
 
@@ -2543,4 +2563,27 @@ export class CompletionProvider {
         // before doing more expensive type evaluation.
         return decl.isMethod && decl.node.decorators.length > 0;
     }
+}
+
+export function printSimplifiedFunctionSignature(functionType: FunctionType): string {
+    // This is a bit arbitrary but works well for us as we have drill-down
+    // to more detailed docs. Better handling of self/cls would be good.
+    const parameters = functionType.details.parameters.filter(
+        (p, index) => !p.hasDefault && !(index === 0 && p.name === 'self')
+    );
+    return (
+        functionType.details.fullName +
+        '(' +
+        parameters
+            .map((p) => {
+                if (p.category === ParameterCategory.VarArgList) {
+                    return '*' + p.name;
+                } else if (p.category === ParameterCategory.VarArgDictionary) {
+                    return '**' + p.name;
+                }
+                return p.name;
+            })
+            .join(', ') +
+        ')'
+    );
 }

packages/pyright-internal/src/tests/fourslash/completions.builtinDocstrings.fourslash.ts
@@ -44,27 +44,27 @@
                 {
                     label: 'object',
                     kind: Consts.CompletionItemKind.Class,
-                    documentation: 'class object()\n\nThis is the class doc for object.',
+                    documentation: 'This is the class doc for object.',
                 },
                 {
                     label: 'A',
                     kind: Consts.CompletionItemKind.Class,
-                    documentation: 'class A()',
+                    documentation: undefined,
                 },
                 {
                     label: 'B',
                     kind: Consts.CompletionItemKind.Class,
-                    documentation: 'class B()\n\nThis is the class doc for B.',
+                    documentation: 'This is the class doc for B.',
                 },
                 {
                     label: 'C',
                     kind: Consts.CompletionItemKind.Class,
-                    documentation: 'class C()\n\nThis is the class doc for C.',
+                    documentation: 'This is the class doc for C.',
                 },
                 {
                     label: 'D',
                     kind: Consts.CompletionItemKind.Class,
-                    documentation: 'class D()',
+                    documentation: undefined,
                 },
             ],
         },
@@ -73,7 +73,7 @@
                 {
                     label: '__init__',
                     kind: Consts.CompletionItemKind.Method,
-                    documentation: '__init__: () -> None\n\nThis is the __init__ doc for object.',
+                    documentation: 'builtins.object.__init__()\n\nThis is the __init__ doc for object.',
                 },
             ],
         },
@@ -82,7 +82,7 @@
                 {
                     label: '__init__',
                     kind: Consts.CompletionItemKind.Method,
-                    documentation: '__init__: () -> None',
+                    documentation: 'builtins.object.__init__()',
                 },
             ],
         },
@@ -91,7 +91,7 @@
                 {
                     label: '__init__',
                     kind: Consts.CompletionItemKind.Method,
-                    documentation: '__init__: () -> None\n\nThis is the __init__ doc for B.',
+                    documentation: 'docstrings.B.__init__()\n\nThis is the __init__ doc for B.',
                 },
             ],
         },
@@ -100,7 +100,7 @@
                 {
                     label: '__init__',
                     kind: Consts.CompletionItemKind.Method,
-                    documentation: '__init__: () -> None',
+                    documentation: 'docstrings.C.__init__()',
                 },
             ],
         },
@@ -109,7 +109,7 @@
                 {
                     label: '__init__',
                     kind: Consts.CompletionItemKind.Method,
-                    documentation: '__init__: () -> None\n\nThis is the __init__ doc for D.',
+                    documentation: 'docstrings.D.__init__()\n\nThis is the __init__ doc for D.',
                 },
             ],
         },

packages/pyright-internal/src/tests/fourslash/completions.fourslash.ts
@@ -38,12 +38,12 @@ await helper.verifyCompletion('exact', 'markdown', {
             {
                 label: 'some_func1',
                 kind: Consts.CompletionItemKind.Function,
-                documentation: '```python\nsome_func1: (a: Unknown) -> None\n```\n---\nsome function docs',
+                documentation: '```python\ntest.some_func1(a)\n```\n---\nsome function docs',
             },
             {
                 label: 'some_func2',
                 kind: Consts.CompletionItemKind.Function,
-                documentation: '```python\nsome_func2: (a: Unknown) -> None\n```\n---\nanother function docs',
+                documentation: '```python\ntest.some_func2(a)\n```\n---\nanother function docs',
             },
         ],
     },

packages/pyright-internal/src/tests/fourslash/completions.inherited.function.docFromStub.fourslash.ts
@@ -21,7 +21,7 @@ await helper.verifyCompletion('included', 'markdown', {
             {
                 label: 'displayhook',
                 kind: Consts.CompletionItemKind.Variable,
-                documentation: '```python\ndisplayhook: () -> Unknown\n```',
+                documentation: undefined,
             },
         ],
     },

packages/pyright-internal/src/tests/fourslash/completions.inherited.overload.docFromScrWithStub.fourslash.ts
@@ -44,8 +44,7 @@ await helper.verifyCompletion('included', 'markdown', {
             {
                 label: 'func',
                 kind: Consts.CompletionItemKind.Method,
-                documentation:
-                    '```python\nfunc(self: ChildA, x: str) -> str\nfunc(self: ChildA, x: int) -> int\n```\n---\nfunc docs',
+                documentation: '```python\noverloads_client.ChildA.func(x)\n```\n---\nfunc docs',
             },
         ],
     },

packages/pyright-internal/src/tests/fourslash/completions.inherited.overload.docFromStub.fourslash.ts
@@ -45,8 +45,7 @@ await helper.verifyCompletion('included', 'markdown', {
             {
                 label: 'func',
                 kind: Consts.CompletionItemKind.Method,
-                documentation:
-                    '```python\nfunc(self: ChildA, x: str) -> str\nfunc(self: ChildA, x: int) -> int\n```\n---\nfunc docs',
+                documentation: '```python\noverloads_client.ChildA.func(x)\n```\n---\nfunc docs',
             },
         ],
     },

packages/pyright-internal/src/tests/fourslash/completions.inherited.property.docFromSrc.fourslash.ts
@@ -88,7 +88,7 @@ await helper.verifyCompletion('included', 'markdown', {
             {
                 label: 'length',
                 kind: Consts.CompletionItemKind.Property,
-                documentation: '```python\nlength: Unknown (property)\n```\n---\nread property doc',
+                documentation: '```python\ntest.ChildGetterDocs.length (property)\n```\n---\nread property doc',
             },
         ],
     },
@@ -97,7 +97,7 @@ await helper.verifyCompletion('included', 'markdown', {
             {
                 label: 'length',
                 kind: Consts.CompletionItemKind.Property,
-                documentation: '```python\nlength: Unknown (property)\n```\n---\nsetter property doc',
+                documentation: '```python\ntest.ChildSetterDocs.length (property)\n```\n---\nsetter property doc',
             },
         ],
     },

commit e6f4432460cdf95e7f00da29e470feea0d5a0248

Fix extraction of parameter docs.
Broken in
https://github.com/microbit-matt-hillsdon/pyright/commit/1e1a476845ed83eec18562cf9a2921b02a0f68e3
where I unintentionally passed the `name = default` instead of the
`name` so it worked for all but defaulted parameters.

packages/pyright-internal/src/languageService/signatureHelpProvider.ts
@@ -134,6 +134,7 @@ export class SignatureHelpProvider {
         let label = functionType.details.fullName + '(';
         const params = functionType.details.parameters.filter((p, index) => !(index === 0 && p.name === 'self'));
         params.forEach((param: FunctionParameter, paramIndex) => {
+            const paramName = param.name || '';
             let paramString: string = param.name || '';
             if (param.category === ParameterCategory.VarArgList) {
                 paramString = '*' + paramString;
@@ -152,7 +153,7 @@ export class SignatureHelpProvider {
                 startOffset: label.length,
                 endOffset: label.length + paramString.length,
                 text: paramString,
-                documentation: extractParameterDocumentation(functionDocString || '', paramString),
+                documentation: extractParameterDocumentation(functionDocString || '', paramName),
             });
 
             label += paramString;